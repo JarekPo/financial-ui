0bd948eaefb08d3750bb71b5dacbc2d6f3910ded
Jaroslaw Polowy <jarekpolowy@gmail.com>
2024-01-29 15:47:59 +0000

    FIN-8: add requests error handling

--- src/services/FMservices.tsx
+++ src/services/FMservices.tsx
@@ -1,14 +1,22 @@
 import {financialModelingInstance} from './instances';
 
 export const getTickerCatalog = () => {
-  financialModelingInstance.get('search-ticker', {params: {query: 'a', exchange: 'ETF'}}).then(({data, status}) => {
-    return data;
-  });
-};
-export const getHistoricalPrices = () => {
-  financialModelingInstance
-    .get('historical-price-full/AAPL', {params: {from: '2023-10-10', to: '2023-10-15'}})
-    .then(({data, status}) => {
+  try {
+    financialModelingInstance.get('search-ticker', {params: {query: 'a', exchange: 'ETF'}}).then(({data, status}) => {
       return data;
     });
+  } catch (error) {
+    console.error('Could not fetch data from the Ticker catalog.', error);
+  }
+};
+export const getHistoricalPrices = () => {
+  try {
+    financialModelingInstance
+      .get('historical-price-full/AAPL', {params: {from: '2023-10-10', to: '2023-10-15'}})
+      .then(({data, status}) => {
+        return data;
+      });
+  } catch (error) {
+    console.error('Could not fetch the historical prices.', error);
+  }
 };
