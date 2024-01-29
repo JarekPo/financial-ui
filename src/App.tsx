69a6c42fdaec5c6aea33728d3f28a88f74192ca6
Jaroslaw Polowy <jarekpolowy@gmail.com>
2024-01-29 14:20:37 +0000

    FIN-3: add axios, create initial API requests handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import Home from './pages/Home';
+
 import './App.css';
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 };
 
 export default App;
