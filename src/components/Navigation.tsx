9afcfd1a7d8845b1f70528f5e358fcd97815a363
Jaroslaw Polowy <jarekpolowy@gmail.com>
2024-02-28 08:53:34 +0000

    FIN-44: move theme selection to the nav bar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,6 +4,7 @@ import {Link} from 'react-router-dom';
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import {useAtom} from 'jotai';
@@ -12,6 +13,7 @@ import logo from '../assets/logo.png';
 import logoDark from '../assets/logo-dark.jpg';
 import {Theme} from '../constants/constants';
 import {themeAtom} from '../state/store';
+import ThemeToggler from './ThemeToggler';
 
 const Navigation = () => {
   const [value, setValue] = useState<number>(1);
@@ -19,26 +21,30 @@ const Navigation = () => {
 
   return (
     <Paper sx={{margin: 1}}>
-      <Stack direction='row'>
-        <Link to='/'>
-          <img
-            src={theme == Theme.light ? logo : logoDark}
-            alt='logo'
-            style={{width: '70px', marginRight: 10, padding: 5}}
-          />
-        </Link>
-
-        <Link to='/'>
-          <Button startIcon={<HomeIcon />} size='large' sx={{padding: 2.5}}>
-            Home
-          </Button>
-        </Link>
-        <Link to='/search-stock'>
-          <Button startIcon={<SearchIcon />} size='large' sx={{padding: 2.5}}>
-            Serch Stock
-          </Button>
-        </Link>
-      </Stack>
+      <Grid container sx={{display: 'flex', justifyContent: 'space-between'}}>
+        <Stack direction='row' justifyContent='space-between' alignItems='center'>
+          <Link to='/'>
+            <img
+              src={theme == Theme.light ? logo : logoDark}
+              alt='logo'
+              style={{width: '70px', marginRight: 10, padding: 5}}
+            />
+          </Link>
+          <Link to='/'>
+            <Button startIcon={<HomeIcon />} size='large' sx={{padding: 2.5}}>
+              Home
+            </Button>
+          </Link>
+          <Link to='/search-stock'>
+            <Button startIcon={<SearchIcon />} size='large' sx={{padding: 2.5}}>
+              Serch Stock
+            </Button>
+          </Link>
+        </Stack>
+        <Grid item sx={{display: 'flex', alignItems: 'center', margin: 1}}>
+          <ThemeToggler />
+        </Grid>
+      </Grid>
     </Paper>
   );
 };
